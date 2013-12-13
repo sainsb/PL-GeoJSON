f8f0af18906ca13e8736b5bd651824397d12abac
Ben Sainsbury <b@redtrails.com>
2013-12-12 18:18:03 -0800

    Support for Multipolygon

--- GETJSONGEOMETRY.sql
+++ GETJSONGEOMETRY.sql
@@ -1,113 +1,40 @@
-create or replace FUNCTION getJSONgeometry(inlayer in varchar2, infield in varchar2, inid in Nvarchar2) return CLOB AS
+create or replace
+FUNCTION getJSONgeometry(inlayer in varchar2, infield in varchar2, inid in Nvarchar2) return CLOB AS
 v_clob CLOB;
-foo varchar2(32000);
-v_ring sde.ST_Linestring;
-temppoint sde.ST_Point;
-numpoints int;
-v_numrings int;
+v_polygon sde.ST_Geometry;
 v_geom sde.ST_Geometry;
-v_tmp_clob CLOB;
-v_vc varchar2(32000);
 SQLCMD VARCHAR2(32000);
-v_x number;
-v_y number;
-
-/* Found here:
-http://www.talkapex.com/2009/06/how-to-quickly-append-varchar2-to-clob.html
-*/
-procedure app(v_clob in out nocopy clob, v_vc in out nocopy varchar2, v_app varchar2) is
-begin
-	v_vc := v_vc || v_app;
-	exception when VALUE_ERROR then
-	if v_clob is null then
-	v_clob := v_vc;
-	else
-	dbms_lob.append(v_clob, v_vc);
-	v_vc := v_app;
-	end if;
-end;
+numpolys number;
+v_type number;
 
 BEGIN
 --select sde.st_transform(shape,4152) into v_geom from taxlots.taxlots where tlid=inid;
 SQLCMD := 'select sde.st_transform(shape,4152) from ' || inLayer || ' WHERE ' || infield ||' = ''' || inid || '''';
 EXECUTE IMMEDIATE SQLCMD into v_geom;
 
-   -- catches all 'no data found' errors
+select sde.st_entity(v_geom) into v_type from dual;
 
-v_vc := v_vc || '"geometry": { "type": "Polygon", "coordinates": [' || chr(10) || '[';
-select sde.st_ExteriorRing(v_geom) into v_ring from dual;
-select sde.st_numpoints(v_ring) into numpoints from dual;
+if v_type = 8 THEN -- POLYGON
 
-for point in 1..(numpoints-1) Loop
+  v_clob := '"geometry": { "type": "Polygon", "coordinates": [' || chr(10);
+  
+  select sde.st_geometryn(v_geom,0) into v_polygon from dual;
+  
+  v_clob := v_clob || getJSONGeometryRings(v_geom) || ']}';
+  
+ELSIF v_type = 264 THEN-- MULTIPOLYGON 
 
-	select sde.st_pointn(v_ring, point) into temppoint from dual;
-	select sde.st_X(temppoint) into v_x from dual;
-	select sde.st_Y(temppoint) into v_y from dual;
-
-	foo := '[' || to_char(v_x) || ',';
-	foo := foo || to_char(v_y) || '],' || chr(10);
-
-	app(v_clob, v_vc, foo);
-end loop;
-
-select sde.st_pointn(v_ring, numpoints) into temppoint from dual;
-select sde.st_X(temppoint) into v_x from dual;
-select sde.st_Y(temppoint) into v_y from dual;
-foo := '['|| to_char(v_x) || ',';
-foo := foo || to_char(v_y) || ']';
-app(v_clob, v_vc, foo);
-
---v_clob := v_clob || v_vc;
-
--- Interior rings
-select sde.st_NumInteriorRing(v_geom) into v_numrings from dual;
-
-if v_numrings > 0 then
-	v_vc := '],' || chr(10) || '[';
-
-	for ring in 1..v_numrings Loop
-		select sde.st_InteriorRingN(v_geom, ring) into v_ring from dual;
-		select sde.st_numpoints(v_ring) into numpoints from dual;
-
-	  for point in 0..numpoints-1 Loop
-	   
-		  select sde.st_pointn(v_ring, point) into temppoint from dual;
-		  select sde.st_X(temppoint) into v_x from dual;
-		  select sde.st_Y(temppoint) into v_y from dual;
-		  
-		  foo := '[' || to_char(v_x) || ',';
-		  foo := foo || to_char(v_y) || '],' || chr(10);
-		  
-		  app(v_clob, v_vc, foo);
-	  end loop;
-	  
-	  select sde.st_pointn(v_ring, numpoints) into temppoint from dual;
-	  select sde.st_X(temppoint) into v_x from dual;
-	  select sde.st_Y(temppoint) into v_y from dual;
-	  foo := '[' || to_char(v_x) || ',';
-	  foo := foo || to_char(v_y) || ']';
-	  
-	  app(v_clob, v_vc, foo);
-	  
-	  if ring <> v_numrings then
-	  foo := '],' || chr(10) || '[';
-	  else
-	  foo :=']';
-	  end if;
-	  
-	  app(v_clob, v_vc, foo);
-
-	End Loop;
-
-else
-	foo :=']';
-	app(v_clob, v_vc, foo);
+  select sde.st_numgeometries(v_geom) into numpolys from dual;
+  v_clob :=  '"geometry": { "type": "MultiPolygon", "coordinates": [';
+  
+  for polygon in 1..(numpolys) Loop
+    select sde.st_geometryn(v_geom, polygon) into v_polygon from dual;
+    v_clob := v_clob || '[' || getJsongeometryrings(v_polygon) || ']';
+    if polygon <> numpolys then
+      v_clob := v_clob || ',';
+    end if;
+  end loop;
+v_clob := v_clob || ']}';
 end if;
-foo:=']}';
-
-app(v_clob, v_vc, foo);
-v_clob := v_clob || v_vc;
-
 return v_clob;
-
 END;
---
+++ GetPolygon.sql
@@ -0,0 +1,71 @@
+create or replace
+FUNCTION getJSONGeometryRings(in_POLYGON in sde.ST_Geometry) return clob as
+v_clob CLOB;
+v_vc varchar2(32000);
+v_ring sde.ST_Linestring;
+numpoints int;
+v_numinteriorrings int;
+temppoint sde.ST_Point;
+v_x number;
+v_y number;
+
+procedure app(v_clob in out nocopy clob, v_vc in out nocopy varchar2, v_app varchar2) is
+begin
+	v_vc := v_vc || v_app;
+	exception when VALUE_ERROR then
+	dbms_lob.append(v_clob, v_vc);
+	v_vc := v_app;
+end;
+
+function point2Char(in_point sde.ST_Point) return varchar2 as
+begin
+select sde.st_X(in_point), sde.ST_Y(in_point) into v_x, v_y from dual;
+return '[' || to_char(v_x,'fm9999999.999999') || ',' || to_char(v_y,'fm9999999.999999') || ']' || chr(10);
+end;
+
+Begin 
+
+/* Exterior ring first */
+select sde.st_ExteriorRing(in_Polygon) into v_ring from dual;
+select sde.st_numpoints(v_ring) into numpoints from dual;
+--dbms_output.put_line(numpoints);
+v_clob := '[' || CHR(10);
+
+for point in 1..(numpoints-1) Loop
+	select sde.st_pointn(v_ring, point) into temppoint from dual;
+	app(v_clob, v_vc, point2char(temppoint) || ',');
+end loop;
+
+/* Final point and closing bracket */
+select sde.st_pointn(v_ring, numpoints) into temppoint from dual;
+app(v_clob, v_vc, point2char(temppoint) || ']');
+
+-- Interior rings
+select sde.st_NumInteriorRing(in_polygon) into v_numinteriorrings from dual;
+
+if v_numinteriorrings > 0 then
+	app(v_clob, v_vc, ',' || chr(10) || '[');
+
+	for ring in 1..v_numinteriorrings Loop --ring 0 is the exterior ring so we start with 1
+		select sde.st_InteriorRingN(in_polygon, ring) into v_ring from dual;
+		select sde.st_numpoints(v_ring) into numpoints from dual;
+
+	  for point in 1..(numpoints-1) Loop
+		  select sde.st_pointn(v_ring, point) into temppoint from dual;
+		  app(v_clob, v_vc, point2char(temppoint) || ',');
+	  end loop;
+	  
+          /* Final point and closing bracket */
+	  select sde.st_pointn(v_ring, numpoints) into temppoint from dual;
+	  app(v_clob, v_vc, point2char(temppoint) || ']');
+	  
+          if ring <> v_numinteriorrings then
+          v_clob := v_clob || ',';
+          end if;
+          
+	End Loop;
+end if;
+
+v_clob := v_clob || v_vc;
+return v_clob;
+end;
